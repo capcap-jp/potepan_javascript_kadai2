17efa56cd16b5345e16121a0e18e4b4cf46bfeaa
capcap-jp <miyamiyacap@gmail.com>
2021-01-04 20:41:37 +0900

    v

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 let result = document.getElementById("result");
 
+// "="と"AC"以外のボタンを押したとき
 function edit(elem) {
   result.value = result.value + elem.value;
 }
 
+// "="ボタンを押したとき
 function calc() {
   result.value = new Function("return " + result.value)();
 }
 
+// "AC"ボタンを押したとき
 function clean() {
   result.value = "";
 }
