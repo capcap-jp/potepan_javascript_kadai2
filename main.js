067fd4cd8e5d36a3619c0c2b50804c6e8ce305e0
capcap-jp <miyamiyacap@gmail.com>
2021-01-04 20:44:46 +0900

    課題提出

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let result = document.getElementById("result");
 
-// "="と"AC"以外のボタンを押したとき
+// '='と'AC'以外のボタンを押したとき
 function edit(elem) {
   result.value = result.value + elem.value;
 }
 
-// "="ボタンを押したとき
+// '='ボタンを押したとき
 function calc() {
   result.value = new Function("return " + result.value)();
 }
 
-// "AC"ボタンを押したとき
+// 'AC'ボタンを押したとき
 function clean() {
   result.value = "";
 }
